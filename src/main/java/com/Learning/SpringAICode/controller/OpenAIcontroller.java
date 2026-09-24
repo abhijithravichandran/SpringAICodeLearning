501a8c240850c09b89711df69469762827ce9531
abhijith <abhijithravichandran61@gmail.com>
2026-09-24 22:48:07 +0530

    We don't have to bind to OpenAI. Instead we can use ChatClient

--- src/main/java/com/Learning/SpringAICode/controller/OpenAIcontroller.java
+++ src/main/java/com/Learning/SpringAICode/controller/OpenAIcontroller.java
@@ -1,7 +1,10 @@
 package com.Learning.SpringAICode.controller;
 
 
+
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,17 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OpenAIcontroller {
 
-    private OpenAiChatModel chatModel;
+    private ChatClient chatClient;
 
     public OpenAIcontroller(OpenAiChatModel chatModel){
-        this.chatModel = chatModel;
+        this.chatClient = ChatClient.create(chatModel);
     }
 
     @GetMapping("api/{message}")
-    public String getAnswers(@PathVariable String message){
+    public ResponseEntity<String> getAnswers(@PathVariable String message){
 
-        String response = chatModel.call(message);
+        String response = chatClient
+                .prompt(message)
+                .call()
+                .content();
 
-        return response;
+        return ResponseEntity.ok(response);
     }
 }
